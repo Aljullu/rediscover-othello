d1cdb15b455b642610506a200b2c102cafec4b3b
Albert Juhe Lluveras <aljullu@gmail.com>
2015-02-07 19:40:14 +0100

    Make sounds deactivated by default

--- js/Sounds.js
+++ js/Sounds.js
@@ -1,9 +1,12 @@
 "use strict";
 
-var Sounds = function () {}
+var Sounds = function () {
+    this.active = false;
+}
 
 
 Sounds.prototype.stoneFlipping = function () {
+    if (!this.active) return;
     var sound = new Audio('./audio/stone-flipping.mp3');
     sound.play();
 }
